1c2848d8e5a03b3185499a2b403946d863436801
batuhanduyuler01 <duyuler15@itu.edu.tr>
2023-01-30 03:57:35 +0300

    genetic algorithm applied isolation mp

--- IsolationMPWrapper.py
+++ IsolationMPWrapper.py
@@ -3,8 +3,10 @@ import pandas as pd
 
 
 class IsolationMatrixProfile(MatrixProfileManager):
-    def __init__(self, df:pd.DataFrame, window_size:int = 60, method = 'mpx'):
+    def __init__(self, df:pd.DataFrame, window_size:int = 60, method = 'mpx',  n_estimators = None, contamination = None):
         super().__init__(df, window_size, method)
+        self.n_estimators = n_estimators or None
+        self.contamination = contamination or None
         
     def calculate_discords(self):
         import numpy as np
@@ -13,7 +15,15 @@ class IsolationMatrixProfile(MatrixProfileManager):
                             for idx, f_idx in enumerate(self.df.columns)}
         
         self.mps_df= pd.DataFrame(curr_mps_dict)
-        model=IsolationForest()
+        n_est = 100
+        cont = "auto"
+
+        if self.n_estimators is not None:
+            n_est = self.n_estimators
+        if self.contamination is not None:
+            cont = self.contamination
+
+        model=IsolationForest(n_estimators = n_est, contamination = cont)
         model.fit(self.mps_df.iloc[:,:])
         
         self.mps_df['scores']=model.decision_function(self.mps_df.iloc[:,:])
@@ -21,3 +31,9 @@ class IsolationMatrixProfile(MatrixProfileManager):
         
     def majority_vote_discords(self):
         self.discords = self.mps_df[self.mps_df["anomaly"] == -1].index.to_list()
+        # discord_window_list = self.mps_df[self.mps_df["anomaly"] == -1].index.to_list()
+        # indice_list = []
+        # for indice in discord_window_list:
+        #     indice_list.extend(list(range(indice, indice + self.window_size - 1)))
+
+        # self.discords = list(set(indice_list))
